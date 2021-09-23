cd48e61253e35c93bebf7072b8d6b5afe9f13e47
DELL <DELL@DESKTOP-39HM29U>
2021-09-23 11:49:32 +0200

    Add project files.

---
+++ Day15RambunctiousRecitation/Part1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15RambunctiousRecitation
+{
+    class Part1
+    {
+        public int calculate(int iterations, string Variables)
+        {
+            //convert string to array 
+            string[] variable = Variables.Split(',');
+            int[] intsVar = Array.ConvertAll(variable, s => int.Parse(s));
+
+            //setting start variables getting first and last positions and new array of 2020
+            int first = 0;
+            int last = intsVar.Last();
+            int[] positions = new int[iterations];
+
+            //adds the initial variables to the array
+            for (int j = 0; j < intsVar.Count(); j++)
+            {
+                positions[intsVar[j]] = ++first;
+            }
+
+            
+            for (int i = first; i < iterations; i++)
+            {
+               int prevpos = positions[last];
+               int next = prevpos != 0 ? first - prevpos : 0;
+               positions[last] = first++;
+               last = next;
+            }
+
+            return last;
+        }
+    }
+}
---
+++ Day15RambunctiousRecitation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day15RambunctiousRecitation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1 part1 = new Part1();
+
+            part1.calculate(30000000, "6,19,0,5,7,13,1");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
